ac891917c09d80ae3bd3933d85024c5977985389
Maciej Paczocha <maciej.paczocha@intel.com>
2021-08-05 11:03:05 +0200

    Fix compilation & extend IF

--- include/memkind/internal/ranking.h
+++ include/memkind/internal/ranking.h
@@ -17,5 +17,8 @@ extern void ranking_add(ranking_t *ranking, ranking_entry_t *entry);
 /// @p entry ownership stays with the caller
 /// @p entry should not be freed until it is removed from ranking
 extern void ranking_remove(ranking_t *ranking, const ranking_entry_t *entry);
-extern double ranking_get_hot_threshold(
+/// get last calculated hot threshold
+extern double ranking_get_hot_threshold(ranking_t *ranking);
+extern double ranking_calculate_hot_threshold(
     ranking_t *ranking, double dram_pmem_ratio);
+extern bool ranking_is_hot(ranking_t *ranking, ranking_entry_t *entry);
--- src/ranking.cpp
+++ src/ranking.cpp
@@ -14,15 +14,16 @@ extern "C" {
 using namespace std;
 
 struct ranking {
-    std::vector<ranking_entry_t> entries; /// ordered hottest -> coldest
+    double hotThreshold;
+    std::vector<ranking_entry_t*> entries; /// ordered hottest -> coldest
 };
 
 // IF to implement:
 // TODO mapping ranking_entry -> RankingEntry
 
 //--------private function implementation---------
-static bool is_hotter(ranking_entry_t a, ranking_entry_t b) {
-    return a.hotness > b.hotness;
+static bool is_hotter(ranking_entry_t *a, ranking_entry_t *b) {
+    return a->hotness > b->hotness;
 }
 
 //--------public function implementation---------
@@ -35,36 +36,45 @@ void ranking_destroy(ranking_t *ranking) {
     delete ranking;
 }
 
+double ranking_get_hot_threshold(ranking_t *ranking) {
+    return ranking->hotThreshold;
+}
+
 /// @pre ranking should be sorted
-double ranking_get_hot_threshold(ranking_t *ranking, double dram_pmem_ratio) {
+double ranking_calculate_hot_threshold(
+    ranking_t *ranking, double dram_pmem_ratio) {
+
     // O(N) -> needs to be upgraded to O(log(N)) by using a tree
     if (ranking->entries.size() == 0)
         return std::numeric_limits<double>::min();
 
     size_t total_size=0;
     for (auto &entry : ranking->entries) {
-        total_size += entry.size;
+        total_size += entry->size;
     }
     size_t threshold_size=dram_pmem_ratio*total_size;
     total_size=0;
     for (auto &entry : ranking->entries) {
-        total_size += entry.size;
+        total_size += entry->size;
         if (total_size >= threshold_size) {
-            return entry.hotness;
+            return entry->hotness;
         }
     }
-    return ranking->entries.back().hotness;
+    ranking->hotThreshold=ranking->entries.back()->hotness;
+    return ranking_get_hot_threshold(ranking);
 }
 
 void ranking_add(ranking_t *ranking, ranking_entry_t *entry) {
     // O(N) -> needs to be upgraded to O(log(N)) by using a tree
-    ranking->entries.push_back(*entry);
+    ranking->entries.push_back(entry);
     sort(ranking->entries.begin(), ranking->entries.end(), is_hotter);
 }
 
+bool ranking_is_hot(ranking_t *ranking, ranking_entry_t *entry) {
+    return entry->hotness > ranking_get_hot_threshold(ranking);
+}
+
 void ranking_remove(ranking_t *ranking, const ranking_entry_t *entry) {
-    // TODO
     auto &t = ranking->entries;
-    t.erase(remove(t.begin(), t.end(), *entry), t.end());
-//     auto find(ranking->entries.begin(), ranking->entries.end(), *entry);
+    t.erase(remove(t.begin(), t.end(), entry), t.end());
 }
