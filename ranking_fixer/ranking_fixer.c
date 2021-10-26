755797f940c300a14159bbd512b41cce77b1dc37
Maciej Paczocha <maciej.paczocha@intel.com>
2021-10-26 15:35:50 +0200

    Create ranking fixer

--- ranking_fixer/ranking_fixer.c
+++ ranking_fixer/ranking_fixer.c
@@ -1,10 +1,8 @@
-
+#include "assert.h"
 
 typedef struct ranking_info {
     double expectedRatioThresh;
     double gain;
-    /// sizeFrame[0]: cold tier size
-    /// sizeFrame[1]: hot tier size
     double hotTierSize;
     double coldTierSize;
 } ranking_info;
@@ -18,24 +16,97 @@ void init_ranking_info(
     info->coldTierSize = expected_ratio_thresh;
 }
 
-double calculate_fixed_ratio(ranking_info *info, double found_ratio) {
+double calculate_fixed_thresh(ranking_info *info, double found_ratio) {
     double diff = found_ratio - info->expectedRatioThresh;
 
-    // |---a----------|-------b--|
-    // |---c--|---------------d--|
-    // |---c--|-(a-c)-|----------|
-    // |---c----------|-e-|------|
+    // case: found thresh too low
+    // |---a----------|--------b--|
+    // |---c--|----------------d--|
+    // |---c--|-(a-c)-|-----------|
+    // |---a----------|-e-|-------|
     // (a-c)/a==e/b
     // e = b/a * (a-c)
+
+    // case: found thresh too high
+    // |---a----------|--------b--|
+    // |---c------------------|-d-|
+    // |---c------------------|-d-|
+    // |---c----------|-(c-a)-|---|
+    // |-------|--e---|-----------|
+    // (c-a)/b==e/a
+    // e = - a/b * (c-a)
     double a=info->coldTierSize;
     double b=info->hotTierSize;
     double c = found_ratio;
     double d = 1-found_ratio;
-    double e = b/a*(a-c);
+    double t=a-c;
 
-    return c+e*info->gain;
+    double e = (t>=0 ? b/a : a/b)*t;
+
+    return a+e*info->gain;
+}
+
+#define assert_close(a, b) do { \
+    const double ACCURACY=1e-9; /* arbitrary value */ \
+    double diff = a-b; \
+    double abs_diff = diff >= 0 ? diff : -diff; \
+    assert(abs_diff < ACCURACY && #a " vs " #b); \
+} while(0)
+
+void test_ratio_transformations(void) {
+    ranking_info info;
+    init_ranking_info(&info, 0.7, 1);
+    double fixed_thresh;
+    // corner cases:
+    // ALL PMEM
+    fixed_thresh = calculate_fixed_thresh(&info, 1);
+    assert_close(fixed_thresh, 0);
+    // ALL DRAM
+    fixed_thresh = calculate_fixed_thresh(&info, 0);
+    assert_close(fixed_thresh, 1);
+    // already correct
+    fixed_thresh = calculate_fixed_thresh(&info, 0.7);
+    assert_close(fixed_thresh, 0.7);
+    // 50%
+    fixed_thresh = calculate_fixed_thresh(&info, 0.85);
+    assert_close(fixed_thresh, 0.35);
+    fixed_thresh = calculate_fixed_thresh(&info, 0.35);
+    assert_close(fixed_thresh, 0.85);
+    // 30%
+    fixed_thresh = calculate_fixed_thresh(&info, 0.8);
+    assert_close(fixed_thresh, 2./3.*0.7);
+    fixed_thresh = calculate_fixed_thresh(&info, 2./3.*0.7);
+    assert_close(fixed_thresh, 0.8);
+}
+
+void test_ratio_transformations_gain(void) {
+    ranking_info info;
+    init_ranking_info(&info, 0.7, 2);
+    double fixed_thresh;
+    // corner cases:
+    // ALL PMEM
+    fixed_thresh = calculate_fixed_thresh(&info, 1);
+    assert_close(fixed_thresh, -0.7);
+    // ALL DRAM
+    fixed_thresh = calculate_fixed_thresh(&info, 0);
+    assert_close(fixed_thresh, 1.3);
+    // already correct
+    fixed_thresh = calculate_fixed_thresh(&info, 0.7);
+    assert_close(fixed_thresh, 0.7);
+    // 50%
+    fixed_thresh = calculate_fixed_thresh(&info, 0.85);
+    assert_close(fixed_thresh, 0);
+    fixed_thresh = calculate_fixed_thresh(&info, 0.35);
+    assert_close(fixed_thresh, 1);
+    // 1/3
+    fixed_thresh = calculate_fixed_thresh(&info, 0.8);
+    assert_close(fixed_thresh, 1./3.*0.7);
+    fixed_thresh = calculate_fixed_thresh(&info, 2./3.*0.7);
+    assert_close(fixed_thresh, 0.9);
 }
 
 int main(int argc, char *argv[]) {
     // TODO write tests
+    test_ratio_transformations();
+    test_ratio_transformations_gain();
 }
