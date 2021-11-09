23945010f7b4b71b536ea4ae5db9ae2922ca0491
Maciej Paczocha <maciej.paczocha@intel.com>
2021-11-09 11:33:29 +0100

    Modify memtier config - most accurate and reliable version

--- include/memkind/internal/memkind_memtier.h
+++ include/memkind/internal/memkind_memtier.h
@@ -288,13 +288,13 @@ extern double old_time_window_hotness_weight;
 
 #define QUANTIFICATION_ENABLED 0
 #define RANKING_FIXER_ENABLED 1
-#define INTERPOLATED_THRESH 0
-#define FALLBACK_TO_STATIC 0
+#define INTERPOLATED_THRESH 1
+#define FALLBACK_TO_STATIC 1
 
 // when buffer is full, waits until it can re-add elements
 // this feature can negativly impact performance!
 #define ASSURE_RANKING_DELIVERY 0
-#define OFFLOAD_RANKING_OPS_TO_BACKGROUD_THREAD 1
+#define OFFLOAD_RANKING_OPS_TO_BACKGROUD_THREAD 0
 
 #if QUANTIFICATION_ENABLED
 typedef int quantified_hotness_t;
